1669c15d3d3563387f153d01908be51dc20333d2
Michael Wyatt <michaelwyatt@protonmail.com>
2018-06-20 18:25:57 -0700

    Renamed TODO file

---
+++ ProjectNotesAndTasks.js
@@ -0,0 +1,75 @@
+/**
+ * Highest Priority Issues
+ * TODO: Figure out image/GIF situation
+ * TODO: Visual Design - Tons of work to be done, animate button presses, cards loading, etc
+ */
+
+/**
+ * Necessary For One Character Demo
+ *
+ *        InitialView/RoundSetup.js:
+ * TODO: Fix bug where "Number of Moves in Round" is based on last round played
+ *
+ *        Cleaning up the code:
+ * TODO: Add a styles object to all pages using multiple sets of styling.
+ *
+ *        filterMoves.js:
+ * TODO: Make Stance Filter work
+ * TODO: Figure out the proper way to implement throw filter with updatedData.json
+ *
+ *        createAllCardsList.js:
+ * TODO: Remove odd choices from cards when they are not the answer (obviously)
+ * TODO: Examples of Odd Move Choices: “null” “KND”, “-”, “0 OC”, “+8(+15)” “+9~+12 GB”, “CS”, “ATD”
+ *
+ *         Visuals/Styling:
+ * TODO: NavBar Dropdowns should be a fixed length with scrollable menu items
+ */
+
+/**
+ * Necessary for All Characters WITHOUT GIFS
+ *
+ * TODO: Add Most Popular tags to each character in updatedData.json
+ * Characters With Most Popular: Akuma - Eliza
+ * TODO: Alisa d/f+4 missing
+ * TODO: Bob f, F+1+2 throw missing
+ * TODO: Bryan d/f+1, 1, 1, 1, 2 missing
+ * TODO: Eddy's unlisted half are not added yet
+ *
+ * TODO: Then, switch over from t7data.json to updatedData.json
+ */
+
+/**
+ * Get around to at some point
+ *
+ *        updatedData.json:
+ * TODO: Remove duplicate moves for the same character. Ex: SS+2
+ *
+ *        InitialView:
+ * TODO: Character select screen with thumbnails
+ * TODO: Character Thumbnail Header at top of the screen with Filter and Property listed
+ *
+ *        RoundSummary:
+ * TODO: Add ability to show date, as well as to show a key value (ie 1, 2, etc)
+ *
+ *        Visuals/Styling:
+ * TODO: RoundHistory - Need a LineGraph to separate User Overall Stats from Rounds Played History
+ */
+
+/**
+ * Style / Color Notes
+ * (Google Image T7 UI, Chrome Dev Tools, Elements, Eye Dropper tool)
+ *
+ * Table row background color Dark: #2c383d    dark grey     NavBar background color
+ * Table row background color Light: #303d43   light grey
+ * Table row text color: #4f5d62   lighter grey
+ * Table row option text color: #03a7df    light blue
+ * Table border color: #01507b             darker light blue
+ * Selected row background color: #025480    medium light blue SAME
+ * Selected row text color: #fefefe          pretty much white
+ * Selected row Border color: #80ffff        light turquoise
+ * Menu font light blue color: #0e9ddc       light blue on dark navy background
+ * Button background color: #025480          medium light blue SAME
+ * Main Menu Background Color: #012a45       dark navy
+ *
+ *
+ */
